2d2d428fcf4f4bf480244b6ac4bad2abe10439cb
Guillaume Desrat <gd.github@bleu04.fr>
2011-08-07 23:06:20 +0200

    created the application to run on Heroku and have the source code hosted at GitHub

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Noskvabbel::Application.config.session_store :cookie_store, :key => '_noskvabbel_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Noskvabbel::Application.config.session_store :active_record_store
