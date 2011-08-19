9378b462ac5015a911853f238bc3c0b95100cda3
Guillaume Desrat <gd.github@bleu04.fr>
2011-08-19 18:21:00 +0200

    added one more passing scenario to display the expenses

--- features/manage_expenses.feature
+++ features/manage_expenses.feature
@@ -9,3 +9,20 @@ Feature: A user manages the expenses
 		And user zifro is in couple
     When I go to the expenses page
     Then I should see "List of expenses"
+
+	Scenario: Displaying expenses on the said page @wip
+		Given I am logged in as zifro
+		And user zifro is in couple
+		And user zifro has recorded following expenses:
+		| label      | amount | spent_on    |
+		| Grocery    | 50.50  | 2011-08-19  |
+		| Restaurant | 63.20  | 2011-08-18  |
+		| Grocery    | 10.60  | 2011-08-17  |
+		| DVD        | 14.90  | 2011-08-16  |
+	  When I go to the expenses page
+	  Then I should see the following expenses:
+		| label      | amount | spent_on    |
+		| Grocery    | 50.50  | 2011-08-19  |
+		| Restaurant | 63.20  | 2011-08-18  |
+		| Grocery    | 10.60  | 2011-08-17  |
+		| DVD        | 14.90  | 2011-08-16  |
--- features/step_definitions/manage_expenses_steps.rb
+++ features/step_definitions/manage_expenses_steps.rb
@@ -6,3 +6,31 @@ Given /^user (\S+) is in couple$/ do |user|
                         :password_confirmation => 'password')
   Couple.create!(:users => [lover1, lover2])
 end
+
+Given /^user (\S+) has recorded following expenses:$/ do |user, table|
+  # table is a Cucumber::Ast::Table
+  user = User.find_by_username(user)
+  table.hashes.each do |expense|
+    Expense.create!(
+      :user     => user,
+      :label    => expense[:label],
+      :amount   => expense[:amount],
+      :spent_on => expense[:spent_on].to_date
+    )
+  end
+end
+
+Then /^I should see the following expenses:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  table.hashes.each do |expense|
+    if page.respond_to? :should
+      expense.values.each do |text|
+        page.should have_content(text)
+      end
+    else
+      expense.values.each do |text|
+        assert page.has_content?(text)
+      end
+    end
+  end
+end
