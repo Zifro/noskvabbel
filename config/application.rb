52a51a57e48ff9f8393783dd1fa62efcfecdaf3b
Guillaume Desrat <gd.github@bleu04.fr>
2012-03-29 22:11:25 +0200

    completed the upgrade to Rails version 3.2.2
    (sort of) followed David Rice's tutorial at
    http://davidjrice.co.uk/2011/05/25/how-to-upgrade-a-rails-application-to
    -version-3-1-0.html

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,9 @@ module Noskvabbel
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+    
+    # Enable the asset pipeline
+    config.assets.enabled = true
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
