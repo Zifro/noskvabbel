0792f0bb71cab31a7189cb4947517c20df793a3a
Guillaume Desrat <gd.github@bleu04.fr>
2012-03-31 23:07:24 +0200

    Enhanced the AJAX deletion to update the balance
    This solves the issue #6

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -9,10 +9,7 @@ class ExpensesController < ApplicationController
   # Lists the expenses recorded for the couple of the user
   def index
     @expenses = current_user.couple.expenses
-    @users = current_user.couple.users.collect do |user|
-        total = user.expenses.inject(0) { |total, e| total += e.amount }
-        { :username => user.username, :total => number_to_currency(total, :unit => '€ ', :separator => ',', :delimiter => ' ') }
-    end      
+    get_users
   end
   
   # Displays an expense
@@ -64,6 +61,7 @@ class ExpensesController < ApplicationController
       @expense = Expense.find(params[:id])
       #TODO: add a filter so that only expenses linked to the couple of the user can be displayed
       if @expense.destroy
+        get_users
         format.html { redirect_to(expenses_path, :notice => 'Expense successfully deleted') }
         format.js
       else
@@ -78,6 +76,13 @@ class ExpensesController < ApplicationController
   def verify_the_user_is_in_couple
     unless current_user.couple
       redirect_to login_path, :notice => "#{flash[:notice]}... sorry, you're not in couple yet"
+    end
+  end
+
+  def get_users
+    @users = current_user.couple.users.collect do |user|
+        total = user.expenses.inject(0) { |total, e| total += e.amount }
+        { :username => user.username, :total => number_to_currency(total, :unit => '€ ', :separator => ',', :delimiter => ' ') }
     end
   end
 
