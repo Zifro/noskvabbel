6c511cfa705da835081bf0d7e1ba448ee9d649cd
Guillaume Desrat <gd.github@bleu04.fr>
2022-09-10 17:02:34 +0200

    Simplify the retrieval of production database data
    This updates the instructions in the SQL query files, to make it simpler
    to retrieve couples, users, and expenses from the current production
    database. The format of the output is also simpler to parse.

--- db/query/retrieve_couples.sql
+++ db/query/retrieve_couples.sql
@@ -1,11 +1,18 @@
 -- To run this on Scalingo PostgreSQL database:
 --
--- 1. Run scalingo -a noskvabbel pgsql-console | cat >couples.txt
--- 2. Copy and paste the query below. It won't echo.
--- 3. Type `exit` (without the backticks), then press Enter key. This will exit.
+-- 1. Run scalingo --app noskvabbel db-tunnel SCALINGO_POSTGRESQL_URL
+-- 2. Set the DATABASE_URL environment variable with what the above command has
+--    instructed you to, using the login and password present in environment
+--    variable SCALINGO_POSTGRESQL_URL. It should be something linke:
+--       postgresql://<user>:<password>@127.0.0.1:<port>/<db name>
+-- 3. Run psql $DATABASE_URL
+-- 4. Run \o couples.txt to output the query results in the file couples.txt
+-- 5. Copy, paste, and execute the query below
+-- 6. Run \q to exist the database client
 --
--- All the output will be in the file couples.txt. Beware it will require some
--- cleanup and a thorough parser to extract information
+-- All the output will be in the file couples.txt, one record per line. Note it
+-- will require some cleanup before parsing, or writing a tool to extract the
+-- fields from the first line, ignore the second line, and drop the last line.
 --
 SELECT  *
 FROM    couples
